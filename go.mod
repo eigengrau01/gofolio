f41c4712f621bcdc1c9e75496d7077c3a54e0735
eigengrau01 <bryantroncosob@gmail.com>
2026-01-25 05:09:03 -0300

    feat: fileserver added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eigengrau01/gofolio
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Server on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
