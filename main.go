080e911f662f735ac9574761b3047def0edd5c9b
eigengrau01 <bryantroncosob@gmail.com>
2026-01-27 08:13:45 -0300

    feat: navbar added

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var static embed.FS
 //go:generate npx tailwindcss build -i static/css/style.css -o static/css/tailwind.css -m
 
 func main() {
-	homePage := components.Index()
+	homePage := components.Navbar()
 	pagesHandler := http.NewServeMux()
 	pagesHandler.Handle("/", templ.Handler(homePage))
 	pagesHandler.Handle("/static/", http.FileServer(http.FS(static)))
